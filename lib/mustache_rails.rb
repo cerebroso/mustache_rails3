ca6bdcd2f1bc3d55d5e5c6347932941b22905715
Rafael Gaspar <rafael.gaspar@gmail.com>
2012-01-10 16:12:31 -0200

    Revert "Fix all the deprecation warnings"
    This reverts commit 71a0d745fa063e99bcd5ae19ed7f04828fbb627f.

--- lib/mustache_rails.rb
+++ lib/mustache_rails.rb
@@ -64,12 +64,17 @@ class Mustache
       end
     end
 
-    class TemplateHandler
+    class TemplateHandler < ActionView::Template::Handler
+
+      include ActionView::Template::Handlers::Compilable
+
+      self.default_format = :mustache
+
       # @return [String] its evaled in the context of the action view
       # hence the hack below
       #
       # @param [ActionView::Template]
-      def call(template)
+      def compile(template)
         mustache_class = mustache_class_from_template(template)
         mustache_class.template_file = mustache_template_file(template)
 
